427a65dac31a9f1a83d65cc3ba338f3565a25c84
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-03-23 20:54:35 +0300

    Deleted unitofwork classes

--- src/SpaTemplates.EntityFrameworkCore/DataSeeder.cs
+++ src/SpaTemplates.EntityFrameworkCore/DataSeeder.cs
@@ -14,66 +14,79 @@ namespace SpaTemplates.EntityFrameworkCore
 
         public static async Task SeedData(SpaTemplatesContext context)
         {
-            if (!context.Users.Any())
-            {
-                var users = new List<ApplicationUser>
-                {
-                    new ApplicationUser
-                    {
-                        Id = Guid.NewGuid(),
-                        AccessFailedCount= 0,
-                        Email = "testuser1@mail.com",
-                        EmailConfirmed = true,
-                        LockoutEnabled = false,
-                        NormalizedEmail = "TESTUSER1@MAIL.COM",
-                        NormalizedUserName = "TESTUSER1",
-                        TwoFactorEnabled = false,
-                        UserName = "testuser1"
-                    },
-                    new ApplicationUser
-                    {
-                        Id = Guid.NewGuid(),
-                        AccessFailedCount= 0,
-                        Email = "testuser2@mail.com",
-                        EmailConfirmed = true,
-                        LockoutEnabled = false,
-                        NormalizedEmail = "TESTUSER2@MAIL.COM",
-                        NormalizedUserName = "TESTUSER2",
-                        TwoFactorEnabled = false,
-                        UserName = "testuser2"
-                    },
-                    new ApplicationUser
-                    {
-                        Id = Guid.NewGuid(),
-                        AccessFailedCount= 0,
-                        Email = "testuser3@mail.com",
-                        EmailConfirmed = true,
-                        LockoutEnabled = false,
-                        NormalizedEmail = "TESTUSER3@MAIL.COM",
-                        NormalizedUserName = "TESTUSER3",
-                        TwoFactorEnabled = false,
-                        UserName = "testuser3"
-                    },
-                };
-
-                context.AddRange(users);
-            }
-
-            if (!context.WeatherForecasts.Any())
-            {
-                var rng = new Random();
-                var weatherForecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
-                {
-                    Id = Guid.NewGuid(),
-                    DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                });
-
-                context.AddRange(weatherForecasts);
-            }
+            SeedUsers(context);
+            SeedWeatherForecasts(context);
 
             await context.SaveChangesAsync();
+        }
+
+        private static void SeedWeatherForecasts(SpaTemplatesContext context)
+        {
+            if (context.WeatherForecasts.Any())
+            {
+                return;
+            }
+
+            var rng = new Random();
+            var weatherForecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Id = Guid.NewGuid(),
+                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+
+            context.AddRange(weatherForecasts);
+        }
+
+        private static void SeedUsers(SpaTemplatesContext context)
+        {
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    Id = Guid.NewGuid(),
+                    AccessFailedCount= 0,
+                    Email = "testuser1@mail.com",
+                    EmailConfirmed = true,
+                    LockoutEnabled = false,
+                    NormalizedEmail = "TESTUSER1@MAIL.COM",
+                    NormalizedUserName = "TESTUSER1",
+                    TwoFactorEnabled = false,
+                    UserName = "testuser1"
+                },
+                new ApplicationUser
+                {
+                    Id = Guid.NewGuid(),
+                    AccessFailedCount= 0,
+                    Email = "testuser2@mail.com",
+                    EmailConfirmed = true,
+                    LockoutEnabled = false,
+                    NormalizedEmail = "TESTUSER2@MAIL.COM",
+                    NormalizedUserName = "TESTUSER2",
+                    TwoFactorEnabled = false,
+                    UserName = "testuser2"
+                },
+                new ApplicationUser
+                {
+                    Id = Guid.NewGuid(),
+                    AccessFailedCount= 0,
+                    Email = "testuser3@mail.com",
+                    EmailConfirmed = true,
+                    LockoutEnabled = false,
+                    NormalizedEmail = "TESTUSER3@MAIL.COM",
+                    NormalizedUserName = "TESTUSER3",
+                    TwoFactorEnabled = false,
+                    UserName = "testuser3"
+                },
+            };
+
+            context.AddRange(users);
         }
     }
 }
