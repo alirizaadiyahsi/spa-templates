6c0b5eba361a9fba7aaedf0ef97c563bae1846d9
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-03-24 17:53:00 +0300

    moved registration to account controller

---
+++ src/SpaTemplates.Web.Server/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SpaTemplates.Domain;
+using SpaTemplates.Web.Server.Helpers;
+using SpaTemplates.Web.Server.ViewModels;
+
+namespace SpaTemplates.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
+            }
+
+            return new OkObjectResult("Account created");
+        }
+    }
+}
--- src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
+++ src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
@@ -1,45 +1,10 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using SpaTemplates.Domain;
-using SpaTemplates.Web.Server.Helpers;
-using SpaTemplates.Web.Server.ViewModels;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SpaTemplates.Web.Server.Controllers
 {
     [Route("api/[controller]")]
     public class TokenAuthController : Controller
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        public TokenAuthController(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody]RegisterViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var applicationUser = new ApplicationUser
-            {
-                UserName = model.UserName,
-                Email = model.Email
-            };
-
-            var result = await _userManager.CreateAsync(applicationUser, model.Password);
-
-            if (!result.Succeeded)
-            {
-                return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
-            }
-
-            return new OkObjectResult("Account created");
-        }
+        
     }
 }
