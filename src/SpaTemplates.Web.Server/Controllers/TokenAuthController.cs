7286801a174125655e29d3c6ced7faaace8a662b
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-03-24 17:24:58 +0300

    Added register action to controller

--- src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
+++ src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SpaTemplates.Domain;
+using SpaTemplates.Web.Server.Helpers;
+using SpaTemplates.Web.Server.ViewModels;
 
 namespace SpaTemplates.Web.Server.Controllers
 {
     [Route("api/[controller]")]
     public class TokenAuthController : Controller
     {
-        //public Task PostAsync([FromBody]JwtRegisterViewModel model)
-        //{
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        //}
+        public TokenAuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Post([FromBody]RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = new ApplicationUser
+            {
+                Email = model.Email,
+                UserName = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
+            }
+
+            return new OkObjectResult("Account created");
+        }
     }
 }
---
+++ src/SpaTemplates.Web.Server/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaTemplates.Web.Server.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
