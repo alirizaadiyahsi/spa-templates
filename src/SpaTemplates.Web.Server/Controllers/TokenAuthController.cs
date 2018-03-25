2f2f9f8916d07afead1445b210ac6f4271322990
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-03-25 16:53:50 +0300

    Added new JWT token based auth

--- src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
+++ src/SpaTemplates.Web.Server/Controllers/TokenAuthController.cs
@@ -1,7 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using SpaTemplates.Domain;
+using SpaTemplates.Web.Server.Authentication.JwtBearer;
 using SpaTemplates.Web.Server.ViewModels;
 
 namespace SpaTemplates.Web.Server.Controllers
@@ -10,17 +16,66 @@ namespace SpaTemplates.Web.Server.Controllers
     public class TokenAuthController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TokenAuthConfiguration _tokenAuthConfiguration;
 
         public TokenAuthController(
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            IOptions<TokenAuthConfiguration> tokenAuthConfiguration)
         {
             _userManager = userManager;
+            _tokenAuthConfiguration = tokenAuthConfiguration.Value;
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Login([FromBody]LoginViewModel credentials)
-        //{
-            
-        //}
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody]LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var userToVerify = await GetClaimsIdentity(loginViewModel.UserName, loginViewModel.Password);
+            if (userToVerify == null)
+            {
+                return Unauthorized();
+            }
+
+            var token = new JwtSecurityToken
+            (
+                issuer: _tokenAuthConfiguration.Issuer,
+                audience: _tokenAuthConfiguration.Audience,
+                claims: userToVerify.Claims,
+                expires: DateTime.UtcNow.AddDays(60),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: _tokenAuthConfiguration.SigningCredentials
+            );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+
+            var userToVerify = await _userManager.FindByNameAsync(userName);
+            if (userToVerify == null)
+            {
+                return null;
+            }
+
+            if (await _userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return new ClaimsIdentity(new GenericIdentity(userName, "Token"), new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, userName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                });
+            }
+
+            return null;
+        }
     }
 }
--- src/SpaTemplates.Web.Server/Startup.cs
+++ src/SpaTemplates.Web.Server/Startup.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using SpaTemplates.Application;
 using SpaTemplates.Domain;
 using SpaTemplates.EntityFrameworkCore;
 using SpaTemplates.Web.Server.ActionFilters;
+using SpaTemplates.Web.Server.Authentication.JwtBearer;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace SpaTemplates.Web.Server
@@ -16,10 +20,14 @@ namespace SpaTemplates.Web.Server
     public class Startup
     {
         public IConfiguration Configuration { get; }
+        private readonly SymmetricSecurityKey _signingKey;
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _signingKey =
+                new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(Configuration["Authentication:JwtBearer:SecurityKey"]));
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -34,6 +42,31 @@ namespace SpaTemplates.Web.Server
 
             services.AddSpaTemplatesEntityFrameworkCore();
             services.AddSpaTemplatesApplication();
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(TokenAuthConfiguration));
+            services.Configure<TokenAuthConfiguration>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(TokenAuthConfiguration.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(TokenAuthConfiguration.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwtBearerOptions =>
+            {
+                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateActor = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Authentication:JwtBearer:Issuer"],
+                    ValidAudience = Configuration["Authentication:JwtBearer:Audience"],
+                    IssuerSigningKey = _signingKey
+                };
+            });
 
             services.AddMvc(options => options.Filters.Add<SpaTemplatesContextActionFilter>());
 
